580f8998baaf1eded5f3d91507523f4ef5553b95
Alain Noel <alain.noel34@gmail.com>
2023-06-14 15:25:04 +0200

    modif text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -50,7 +50,7 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portofolio</span>
+            <span className="tagline">Welcome to my Portfolio</span>
               <h1>{`Hi ! I'm Alan`} <span className="wrap">{text}</span></h1>
             <p>After more than 5 years' experience in operations and communications, both in a start-up and in a large group, I decided to move into the Web Developer profession.
 Passionate about tech, I design web projects independently. I love experimenting, discovering and learning new technologies.</p>
