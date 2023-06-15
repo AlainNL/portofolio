25901eb8b9eb39e408c6a60517a23a078df29e40
Alain Noel <alain.noel34@gmail.com>
2023-06-15 16:27:33 +0200

    modif effect for banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,49 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap"
 import headerImg from "../assets/header-img.svg";
+import TextScramble from "./TextScramble";
 
+const phrases = [
+  "- Web Developer"
+];
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const toRotate = [ " - Full stack Developer"]
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta]= useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const period = 1000;
+
+  const textRef = useRef(null);
+  const fx = useRef(null);
 
   useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
+    fx.current = new TextScramble(textRef.current);
+    let counter = 0;
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    const next = () => {
+      fx.current.setText(phrases[counter]).then(() => {
+        setTimeout(next, 3000);
+      });
+      counter = (counter + 1) % phrases.length;
+    };
 
-    const tick = () => {
-      let i = loopNum % toRotate.length;
-      let fullText = toRotate[i];
-      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 2);
-
-      setText(updatedText);
-
-      if (isDeleting) {
-        setDelta(prevDelta => prevDelta / 2);
-      }
-
-      if (!isDeleting && updatedText === fullText) {
-        setIsDeleting(true);
-        setIndex(prevIndex => prevIndex - 1);
-        setDelta(period);
-      } else if (isDeleting && updatedText === '') {
-        setIsDeleting(false);
-        setLoopNum(loopNum +1);
-        setIndex(1);
-        setDelta(500);
-      } else {
-        setIndex(prevIndex => prevIndex + 1);
-      }
-    }
+    next();
+  }, []);
 
   return (
     <section className="banner" id="home">
@@ -51,7 +32,7 @@ export const Banner = () => {
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
-              <h1>{`Hi ! I'm Alan`} <span className="wrap">{text}</span></h1>
+              <h1>{`Hi ! I'm Alan`} <span className="wrap" ref={textRef}></span></h1>
             <p>After more than 5 years' experience in operations and communications, both in a start-up and in a large group, I decided to move into the Web Developer profession.
 Passionate about tech, I design web projects independently. I love experimenting, discovering and learning new technologies.</p>
           </Col>
