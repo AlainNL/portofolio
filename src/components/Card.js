806ac1e580d79ce0c9965fca8eb7e02355d89f4f
Alain Noel <alain.noel34@gmail.com>
2024-09-10 12:20:46 +0200

    modiff css card

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export const Card = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-      <img src={imgUrl} alt="img Url"/>
+      <img src={imgUrl} alt={title} style={{ width: "100%", height: "260px" }} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
